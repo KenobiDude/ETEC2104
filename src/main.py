e2f2f2a6c79c25658ee7d7fbd90b6cc714bdb2f1
brant_harris <bcharris917@gmail.com>
2025-01-16 14:40:27 -0500

    changed main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
-import cherrypy
-import mako.template
-import os.path
+#import cherrypy
+#import mako.template
+#import os.path
 
-PYPATH = os.path.dirname(__file__)
+#PYPATH = os.path.dirname(__file__)
+#
+#class App:
+#    @cherrypy.expose
+#    def index(self):
+#        return "I like pie!"
 
-class App:
-    @cherrypy.expose
-    def index(self):
-        return "I like pie!"
+#    @cherrypy.expose
+#    def test(self):
+#        t = mako.template.Template(filename=f"{PYPATH}/../html/test.html")
+#        return t.render(foobar=42)
 
-    @cherrypy.expose
-    def test(self):
-        t = mako.template.Template(filename=f"{PYPATH}/../html/test.html")
-        return t.render(foobar=42)
+#app = App()
+#cherrypy.quickstart(app)
 
-app = App()
-cherrypy.quickstart(app)
+print("Hello!")
